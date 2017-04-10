f487e1a785c0fb48e0f168193c2e1ac3d3512248
Nichlas <nichlas_vanggaard@hotmail.com>
2017-04-10 13:22:11 +0200

    report handling

--- pyprofiler/core/__init__.py
+++ pyprofiler/core/__init__.py
@@ -3,3 +3,4 @@ from pyprofiler.core.util import Event
 from pyprofiler.core.ws import WebSocketHandler
 from pyprofiler.core.ib import InformationBase
 from pyprofiler.core.sniff import SnifferHandler
+from pyprofiler.core.report import ReportHandler
--- pyprofiler/core/sniff.py
+++ pyprofiler/core/sniff.py
@@ -6,38 +6,28 @@ import asyncio
 import pysniffer.l4.tcp
 
 def create_sniffer():
-    return pysniffer.core.Application(ifname='lo')
+    return pysniffer.core.Application(ifname='enp0s25')
 
 class SnifferHandler:
 
-    def __init__(self, sniffer, loop, ws, ib):
+    def __init__(self, sniffer, loop, ws, ib, reportHandler):
         self.sniffer = sniffer
         self.loop = loop
         self.ws = ws
         self.ib = ib
         self.sniffer.onReport += self.onReportBridge
+        self.reportHandler = reportHandler
 
     def onReportBridge(self, *args):
         self.loop.call_soon_threadsafe(self.onReport, *args)
     
     def onReport(self, caller, report):
-        if isinstance(report, pysniffer.l4.tcp.OpenPortReport):
-            #self.ws.emit_all(['report', report.__dict__])
-            if not 'devices' in self.ib.root:
-                self.ib.root['devices'] = []
-            
-            dev = next((x for x in self.ib.root['devices'] if x['name'] == report.host), None)
-            if not dev:
-                dev = {'name': report.host, 'ports':[report.port]}
-                self.ib.root['devices'].append(dev)
-            else:
-                if report.port not in dev['ports']:
-                    dev['ports'].append(report.port)
+        self.reportHandler.handleReport(report)
 
     @staticmethod
     def register(app):
         app.singleton(pysniffer.core.Application, create_sniffer)
-        app.singleton(SnifferHandler, SnifferHandler, pysniffer.core.Application, asyncio.BaseEventLoop, pyprofiler.core.WebSocketHandler, pyprofiler.core.InformationBase)
+        app.singleton(SnifferHandler, SnifferHandler, pysniffer.core.Application, asyncio.BaseEventLoop, pyprofiler.core.WebSocketHandler, pyprofiler.core.InformationBase, pyprofiler.core.ReportHandler)
     
     def boot(self):
         self.thread = threading.Thread(target=self.sniffer.run)
