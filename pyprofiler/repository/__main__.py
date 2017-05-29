50792a3f202d9d39daf541f527422d315aea1477
Daniel Amkaer Sorensen <daniel.amkaer@gmail.com>
2017-05-29 17:09:57 +0200

    added repository code

--- pyprofiler/repository/__main__.py
+++ pyprofiler/repository/__main__.py
@@ -12,18 +12,38 @@ try:
 except IOError:
     m = Model()
 
-profile = json.load(sys.stdin)
-features = features_from_profile(profile)
-model_features = m.fromFeatures(features)
-cross = m.crossProduct(model_features)
-if cross.shape[0] > 0:
-    print(f"cross: {cross}")
-    i = np.argmax(cross)
-    print(f"i: {i}")
-    if cross[i] != 1:
+def train():
+    profile = json.load(sys.stdin)
+    features = features_from_profile(profile)
+    model_features = m.fromFeatures(features)
+    cross = m.crossProduct(model_features)
+    if cross.shape[0] > 0:
+        print(f"cross: {cross}")
+        i = np.argmax(cross)
+        print(f"i: {i}")
+        if cross[i] != 1:
+            m.addProfile(model_features)
+    else:
         m.addProfile(model_features)
-else:
-    m.addProfile(model_features)
+
+def list():
+    for id,profile in enumerate(m.data):
+        print(f"Profile {id}")
+        for f,val in enumerate(profile):
+            if val:
+                print(f"\tFeature {f}: {m.features[f]}")
+
+        print()
+
+commands = {'list': list, 'train': train}
+try:
+    command = commands[sys.argv[1]]
+    command()
+except IndexError:
+    print(f"command should be specified using: {sys.argv[0]} <command>. valid ones: {', '.join(commands.keys())}")
+except KeyError:
+    print(f"invalid command. valid ones: {', '.join(commands.keys())}")
+
 
 with open('model.bin','wb') as file:
     pickle.dump(m, file)
